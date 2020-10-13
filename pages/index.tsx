6583c742414f25cd6b819a0302cf7342ef6b9c9d
Dave van Hoorn <yo@davevanhoorn.com>
2020-10-13 08:57:08 +0100

    Responsive index hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,9 @@ import Logos from '~/components/Logos/Logos'
 
 const Index: FunctionComponent = () => (
   <IndexLayout>
-    <main className="px-1 mt-12">
-      <div className="w-4/6 flex items-center mx-auto">
-        <div className="w-3/6 pr-12">
+    <main className="sm:px-1 sm:mt-12">
+      <div className="relative w-12/12 md:w-10/12 lg:w-4/6 flex flex-col-reverse sm:flex-row items-center mx-auto">
+        <div className="w-12/12 sm:w-3/6 pr-6 xl:pr-12">
           <h1 className="text-4xl font-bold">Hi, ik ben Dave</h1>
           <p className="text-xl mt-6">
             Ik werk vanuit Portugal als freelance developer voor bedrijven als Noten.nl,
@@ -30,7 +30,7 @@ const Index: FunctionComponent = () => (
             <span role="img" aria-label="Portugese vlag">🇵🇹</span>
           </p>
         </div>
-        <div className="w-3/6 p-8">
+        <div className="absolute sm:relative top-0 right-0 w-5/12 sm:w-3/6 p-4 xl:p-8 mb-4 opacity-25 sm:opacity-100">
           <img className="w-full" src="./davevanhoorn.jpg" alt="Dave van Hoorn van Dave.nl" />
         </div>
       </div>
