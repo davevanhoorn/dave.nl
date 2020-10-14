7be3ad0efddd9b1c960bbedd4f391fde0d653f4e
Dave van Hoorn <yo@davevanhoorn.com>
2020-10-14 17:18:37 +0100

    Animate header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
 // EXTERNAL
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
@@ -10,6 +11,17 @@ const Header = () => {
   const handleToggleMenu = () => {
     setMenuIsVisible((currentState) => !currentState)
   }
+
+  const navVariants = {
+    hidden: { x: menuIsVisible ? 100 : 0 },
+    show: { x: 0 },
+  }
+
+  const overlayVariants = {
+    hidden: { opacity: 0 },
+    show: { opacity: 0.5 },
+  }
+
   return (
     <>
       <div className="bg-red-400 text-center py-4 text-white">Deze site is in ontwikkeling.</div>
@@ -22,32 +34,42 @@ const Header = () => {
               </a>
             </Link>
           </div>
-          <nav className={`${menuIsVisible ? 'block fixed top-0 right-0 w-auto bg-black h-full flex flex-col items-start p-4 z-30' : 'hidden lg:block'}`}>
+          <motion.nav
+            transition={{ ease: 'circOut' }}
+            variants={navVariants}
+            initial="hidden"
+            animate={menuIsVisible ? 'show' : 'hidden'}
+            className={`${menuIsVisible ? 'block fixed top-0 right-0 w-auto bg-teal-400 shadow-lg h-full flex flex-col items-start p-4 z-30' : 'hidden lg:block'}`}
+          >
             <Link href="/wordpress-websites">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>WordPress optimalisatie</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 font-medium text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>WordPress optimalisatie</a>
             </Link>
             <Link href="/react-typescript-werk">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>React & TypeScript</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 font-medium text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>React & TypeScript</a>
             </Link>
             <Link href="/over-mij">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>Over mij</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 font-medium text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>Over mij</a>
             </Link>
             <Link href="/contact">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'mr-2 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>Contact</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 font-medium text-white' : 'mr-2 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded-sm'}`}>Contact</a>
             </Link>
             <Link href="/samenwerken">
-              <a className={`p-2 px-3 rounded-sm ${menuIsVisible ? 'bg-black text-white border-yellow-500 bg-yellow-500' : 'bg-transparent bg-teal-400 text-white hover:border-transparent'}`}>Samenwerken</a>
+              <a className={`p-2 rounded ${menuIsVisible ? 'border-orange-500 text-orange-500 bg-white px-2 font-medium' : 'bg-transparent bg-teal-400 text-white hover:border-transparent'}`}>Samenwerken</a>
             </Link>
             {menuIsVisible && (
-              <button type="button" className="flex justify-end items-end flex-grow mr-4 xl:mr-1 py-2 bg-black text-white hover:bg-black hover:text-white rounded text-sm font-bold mt-2" onClick={handleToggleMenu}>Menu sluiten</button>
+              <button type="button" className="flex justify-end items-end flex-grow mr-4 xl:mr-1 py-2 text-white rounded text-sm mt-2" onClick={handleToggleMenu}>Menu sluiten</button>
             )}
-          </nav>
-          <button type="button" className="block lg:hidden p-2 px-3 bg-black text-white rounded" onClick={handleToggleMenu}>Menu</button>
+          </motion.nav>
+          <button type="button" className="block lg:hidden p-2 px-3 bg-teal-400 text-white rounded" onClick={handleToggleMenu}>Menu</button>
           {menuIsVisible && (
-            <div
+            <motion.div
+              transition={{ ease: 'circOut' }}
+              variants={overlayVariants}
+              initial="hidden"
+              animate={menuIsVisible ? 'show' : 'hidden'}
               role="none"
               onClick={handleToggleMenu}
-              className={`fixed w-full h-full top-0 left-0 bg-white z-20 ${menuIsVisible ? 'opacity-75' : 'opacity-0'}`}
+              className={`fixed w-full h-full top-0 left-0 bg-black z-20 ${menuIsVisible ? 'opacity-75' : 'opacity-0'}`}
             />
           )}
         </header>
