22ecd576dc62aa9cea765538df9d51576b6ed6da
Dave van Hoorn <yo@davevanhoorn.com>
2020-10-14 11:05:01 +0100

    leading & font weights on hero

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -14,32 +14,32 @@ const Header = () => {
     <>
       <div className="bg-red-200 text-center p-4 text-red-800">Deze website is in ontwikkeling.</div>
       <div className={styles.headerWrapper}>
-        <header className="py-4 lg:py-8 flex justify-between items-center text-lg text-black font-medium leading-none px-3 max-w-screen-lg mx-auto p-4">
+        <header className="py-4 lg:py-8 flex justify-between items-center text-lg text-black leading-none px-3 max-w-screen-lg mx-auto p-4">
           <div>
             <Link href="/">
-              <a className="p-2 px-1 text-black font-bold">
+              <a className="p-2 px-1 text-black">
                 Dave.nl
               </a>
             </Link>
           </div>
           <nav className={`${menuIsVisible ? 'block fixed top-0 right-0 w-auto bg-black h-full flex flex-col items-start p-4 z-30' : 'hidden lg:block'}`}>
             <Link href="/wordpress-websites">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'mr-1 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>WordPress optimalisatie</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>WordPress optimalisatie</a>
             </Link>
             <Link href="/react-typescript-werk">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'mr-1 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>React & TypeScript</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>React & TypeScript</a>
             </Link>
             <Link href="/over-mij">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'mr-1 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>Over mij</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>Over mij</a>
             </Link>
             <Link href="/contact">
-              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'mr-4 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>Contact</a>
+              <a className={`${menuIsVisible ? 'py-2 mb-4 bg-black text-white' : 'mr-2 p-2 px-3 bg-white text-black hover:bg-black hover:text-white rounded'}`}>Contact</a>
             </Link>
             <Link href="/samenwerken">
               <a className={`p-2 px-3 rounded ${menuIsVisible ? 'bg-black text-white border-orange-500 bg-orange-500' : 'bg-transparent hover:bg-orange-500 text-orange-500 hover:text-white border border-orange-500 hover:border-transparent'}`}>Samenwerken</a>
             </Link>
             {menuIsVisible && (
-              <button type="button" className="flex justify-end items-end flex-grow mr-4 xl:mr-1 py-2 bg-black text-white hover:bg-black hover:text-white rounded text-sm font-medium mt-2" onClick={handleToggleMenu}>Menu sluiten</button>
+              <button type="button" className="flex justify-end items-end flex-grow mr-4 xl:mr-1 py-2 bg-black text-white hover:bg-black hover:text-white rounded text-sm font-bold mt-2" onClick={handleToggleMenu}>Menu sluiten</button>
             )}
           </nav>
           <button type="button" className="block lg:hidden p-2 px-3 bg-black text-white rounded" onClick={handleToggleMenu}>Menu</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,43 +18,49 @@ const Index: FunctionComponent = () => (
     <IndexLayout>
       <main className="sm:px-1 sm:mt-6">
         <div className="relative w-12/12 md:w-10/12 lg:w-11/12 flex flex-col-reverse sm:flex-row items-center mx-auto">
-          <div className="w-12/12 sm:w-3/6 pr-6 xl:pr-12 text-xl">
-            <h1 className="text-4xl font-bold leading-none">
+          <div className="w-12/12 sm:w-3/6 pr-6 xl:pr-12 text-lg">
+            <h1 className="text-4xl font-medium leading-none">
+              Hallo, Dave hier
+              {' '}
               <span role="img" aria-label="Zwaaiende hand">👋</span>
-              {' '}
-              Hallo, Dave hier!
-              {' '}
             </h1>
-            <p className="mt-6">
+            <p className="mt-6 leading-relaxed">
               Vanuit Portugal werk ik als freelance web developer voor bedrijven als Noten.nl,
               Capgemini, Schiphol en ReisReport.nl.
             </p>
-            <p className="mt-6">
-              Ik help opdrachtgevers met het
+            <p className="mt-6 leading-relaxed">
+              Ik help met
               {' '}
               <Link href="/wordpress-websites">
-                <a className="hover:underline font-bold">WordPress optimalisatie</a>
+                <a className="border-bottom border-dotted border-solid border-b-2 border-black">WordPress optimalisatie</a>
               </Link>
               , het maken van
               {' '}
               <Link href="/woocommerce-plugins">
-                <a className="hover:underline font-bold">WooCommerce plugins</a>
+                <a className="border-bottom border-dotted border-solid border-b-2 border-black">WooCommerce plugins</a>
               </Link>
               {' '}
-              of het schrijven van full-stack
-              apps in
+              of het schrijven van full-stack apps in
               {' '}
               <Link href="/full-stack-mvp">
-                <a className="hover:underline font-bold">
+                <a className="border-bottom border-dotted border-solid border-b-2 border-black">
                   React en TypeScript
                 </a>
               </Link>
               .
             </p>
-            <p className="mt-6">
-              Samenwerken? Stuur een WhatsApp of een email en ik kom snel bij je terug.
+            <p className="mt-6 leading-relaxed">
+              Samenwerken? Neem contact op via
+              {' '}
+              <a className="border-bottom border-dotted border-solid border-b-2 border-black" href="https://wa.me/31616910079" rel="noopener noreferrer" target="_blank">WhatsApp</a>
+              {' '}
+              of per
+              {' '}
+              <a href="mailto:mail@dave.nl" className="border-bottom border-dotted border-solid border-b-2 border-black">email</a>
+              {' '}
+              en ik kom snel bij je terug.
             </p>
-            <p className="mt-6">
+            <p className="mt-7 leading-relaxed">
               Tenha um bom dia (heb een fijne dag)
               {' '}
               <span role="img" aria-label="Portugese vlag">🇵🇹</span>
