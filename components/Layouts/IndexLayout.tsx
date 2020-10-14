e6b556b015002cca10c4413301f5db09dac52a90
Dave van Hoorn <yo@davevanhoorn.com>
2020-10-14 16:52:03 +0100

    Responsiveness

--- components/Layouts/IndexLayout.tsx
+++ components/Layouts/IndexLayout.tsx
@@ -7,7 +7,7 @@ import Header from '~/components/Header/Header'
 const IndexLayout: FunctionComponent = ({ children }) => (
   <>
     <Header />
-    <div className="max-w-screen-lg mx-auto p-4">
+    <div className="max-w-screen-lg mx-auto p-4 pb-0 sm:pb-8">
       {children}
     </div>
   </>
