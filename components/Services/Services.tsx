3d4d67d3060c6ed032e9170987890a62db11a311
Dave van Hoorn <yo@davevanhoorn.com>
2020-10-15 22:06:05 +0100

    Mobile font sizes for services

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -7,7 +7,7 @@ const Services = () => (
     <h2 className="p-4 text-4xl font-bold leading-tight text-center mt-8 md:mt-10 md:mb-16">Dit kan ik voor je doen</h2>
     <div className="flex flex-wrap max-w-screen-lg mx-auto lg:mt-12 text-lg leading-relaxed font-medium">
       <div className="p-4 md:p-0 w-full md:w-6/12 md:pl-24 md:pr-4 md:mb-12">
-        <h6 className="text-3xl mb-2 font-bold leading-tight">WordPress optimalisatie</h6>
+        <h6 className="text-2xl md:text-3xl mb-2 font-bold leading-tight">WordPress optimalisatie</h6>
         <p className="mb-2">
           Ik kan jouw WordPress site sneller maken,
           {' '}
@@ -18,7 +18,7 @@ const Services = () => (
         </Link>
       </div>
       <div className="p-4 md:p-0 md:w-6/12 md:pr-12 md:pl-12 md:mb-12">
-        <h6 className="text-3xl mb-2 font-bold leading-tight">WooCommerce maatwerk</h6>
+        <h6 className="text-2xl md:text-3xl mb-2 font-bold leading-tight">WooCommerce maatwerk</h6>
         <p className="mb-2">
           Ik kan een plugin schrijven voor jouw shop,
           {' '}
@@ -29,7 +29,7 @@ const Services = () => (
         </Link>
       </div>
       <div className="p-4 md:p-0 md:w-6/12 md:pl-24 md:pr-4 md:mb-12">
-        <h6 className="text-3xl mb-2 font-bold leading-tight">Een &quot;MVP&quot; maken</h6>
+        <h6 className="text-2xl md:text-3xl mb-2 font-bold leading-tight">Een &quot;MVP&quot; maken</h6>
         <p className="mb-2">
           Ik kan een minimum viable product maken,
           {' '}
@@ -40,7 +40,7 @@ const Services = () => (
         </Link>
       </div>
       <div className="p-4 md:p-0 md:w-6/12 md:pr-12 md:pl-12 md:mb-12">
-        <h6 className="text-3xl mb-2 font-bold leading-tight">Een kapotte site repareren</h6>
+        <h6 className="text-2xl md:text-3xl mb-2 font-bold leading-tight">Een kapotte site repareren</h6>
         <p className="mb-2">
           Ik kan jouw SSL certificaat vernieuwen,
           {' '}
