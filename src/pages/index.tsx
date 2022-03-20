49d25bd5610c7321cd737140c6393630b06fb747
Dave van Hoorn <yo@dave.nl>
2022-03-20 11:22:10 +0100

    feat: improve intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,33 +44,35 @@ export default function HomePage() {
       <main>
         <div className='grid md:grid-cols-12 md:gap-8'>
           <div className='col-span-12 md:col-span-8 md:border-r lg:col-span-9'>
-            <div className='mb-8 rounded-lg border-2 border-black bg-yellow-100 p-5 text-sm font-medium leading-6 md:mr-8'>
-              <span className='font-semibold'>About me in 62 words:</span>{' '}
-              <br />
-              Front-end developer based in Utrecht, the Netherlands. With{' '}
-              <span className='relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-green-200'>
+            <div className='text-md mb-8 rounded-lg border-2 border-black bg-yellow-100 p-5 font-medium leading-7 md:mr-8'>
+              <span className='text-lg font-semibold'>Hi! 🖖</span> <br />
+              I&apos;m Dave, a Dutch front-end developer based in the
+              Netherlands. With{' '}
+              <span className='relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-green-200 '>
                 <span className='relative font-semibold text-black'>
                   10+ years
                 </span>
               </span>{' '}
               of experience I&apos;m grateful to have worked for both local and
-              international brands. Schiphol, Noten.nl, Dept Agency, Capgemini
-              and Euroclear to name a few. In my recent jobs I focus on FE
-              development with Next.js,{' '}
+              international brands like Schiphol, Noten.nl, Dept Agency,
+              Capgemini and Euroclear. I focus on front-end development with
+              Next.js,{' '}
               <span className='relative inline-block before:absolute before:-inset-1 before:block before:-skew-y-1 before:bg-red-200'>
                 <span className='relative font-semibold text-black'>React</span>
               </span>{' '}
-              and TypeScript. Always in for a chat about music,{' '}
+              and TypeScript. <br />
+              <br /> Always in for a chat about{' '}
               <span className='relative inline-block before:absolute before:-inset-1 before:block before:skew-y-1 before:bg-blue-200'>
                 <span className='relative font-semibold text-black'>dogs</span>
               </span>{' '}
-              , surfing, skateboarding, mountainbiking and the web.
+              , surfing, skateboarding, mountainbiking and your Spotify
+              playlist.
             </div>
             <h3 className='text-2xl font-semibold leading-7'>Experience</h3>
             <Experience
               title='Freelance front-end developer @ Dept Agency'
-              dates='01-2022 - present'
-              description='Develop a component based front-end with a headless Umbraco CMS based on Figma designs. Work in a 100% remote Agile team with two FE developers, one BE developer, two UX / UI designers and multiple project managers. Client faced project for a well known insurance company in the Netherlands.'
+              dates='January 2022 - March 2022'
+              description='Develop a component based front-end with a headless Umbraco CMS based on Figma designs. Work in a 100% remote Agile team with two FE developers, one BE developer, two UX / UI designers and multiple project managers. Brownfield, client-faced project for a well known insurance company in the Netherlands.'
               stack='React, TypeScript, Next.js, GraphQL, Chakra UI, Azure DevOps, Git, Figma'
               rate='€80/hour'
               location='Utrecht, Netherlands (100% remote)'
@@ -79,8 +81,8 @@ export default function HomePage() {
 
             <Experience
               title='Freelance front-end developer @ Bankai'
-              dates='01-2021 - 03-2021'
-              description="Develop customer flow based on designs. Enable the user to add car by license plate with external API\'s (or manually) and calculate efficient BPM models within UsedCarController.nl."
+              dates='January 2021 - March 2021'
+              description="Develop customer flow based on designs. Enable the user to add car by license plate with external API's (or manually) and calculate efficient BPM models within UsedCarController.nl."
               stack='React, Redux, Formik, Axios, PHP, Netlify, GitLab, ClickUp, Git, Figma'
               rate='€70/hour'
               location='Utrecht, Netherlands (100% remote)'
@@ -88,9 +90,9 @@ export default function HomePage() {
             />
 
             <Experience
-              title='Full-stack developer @ Noten.nl'
-              dates='01-2018 - 12-2021'
-              description='Develop the headless front-end and custom WordPress (with GraphQL) back-end of Noten.nl, NüsseKaufen.de. I was responsible for design, development, marketing etc. Exited the business after organically scaling to five figures.'
+              title='Owner | full-stack developer @ Noten.nl'
+              dates='January 2018 - December 2021'
+              description='Develop the headless frontend and custom WordPress backend (with GraphQL) of Noten.nl and NüsseKaufen.de. I was responsible for design, development, photography, marketing and SEO. Scaled to five figures a month (without running ads) and exited the business.'
               stack='React, TypeScript, Next.js, Tailwind CSS, PHP, Vercel, Git'
               location='Cape Town, South Africa | Utrecht, Netherlands'
               industry='B2C | food'
@@ -98,7 +100,7 @@ export default function HomePage() {
 
             <Experience
               title='Front-end developer @ Euroclear Belgium'
-              dates='08-2019 - 05-2020'
+              dates='August 2019 - May 2020'
               description='Translate designs into reusable, custom styled Material-UI components for the Euroclear InvestorInsight® webapp. This webapp enables corporations to gain insights in the structure of their shareholders and intermediaries to comply with SRD II obligations.'
               stack='React, TypeScript, .NET, Material-UI, Azure Devops, Formik, Git, Figma'
               location='Amsterdam, Netherlands'
@@ -107,7 +109,7 @@ export default function HomePage() {
 
             <Experience
               title='Front-end developer @ Schiphol Nederland (for Orange Tribes)'
-              dates='05-2018 - 08-2018'
+              dates='May 2018 - August 2018'
               description='Create an interactive map for Integral Safety Schiphol (.com). The map should be manageable by Schiphol employees. I built a custom, multilingual WordPress website with jQuery, Leaflet.js. The map can be zoomed in and out of by using pre-exported tiles based on a SVG illustration.'
               stack='WordPress, Advanced Custom Fields, jQuery, Leaflet.js, Git'
               location='Bali, Indonesia (100% remote)'
@@ -119,11 +121,11 @@ export default function HomePage() {
             </h4>
             <Certificate
               title='Professional Scrum Master PSMI'
-              issuedDate='11-2019'
+              issuedDate='November 2019'
             />
             <Certificate
               title='HBO Bachelor of Commerce (B.Comm.), Communication & Media Design'
-              issuedDate='05-2013'
+              issuedDate='May 2013'
             />
             <div className='mt-8 flex items-center justify-between border-t pt-6 pr-8'>
               <h4 className='text-2xl font-semibold leading-7'>About</h4>
@@ -138,15 +140,13 @@ export default function HomePage() {
 
             <div className='mt-4 mb-2 border-b pb-4 leading-7 last:border-b-0 md:mr-8'>
               <p className='mb-5'>
-                Hi! 👋 I&apos;m Dave, a 33 year old web enthousiast.{' '}
+                When I&apos;m not working I enjoy walking my (Balinese) dog,
+                shredding Dutch trails on my mountainbike (these can actually be
+                challenging) or surfing small to medium sized waves, preferably
+                in warm water.
               </p>
               <p className='mb-5'>
-                Outside of work I enjoy walking my Balinese dog, shredding Dutch
-                trails on my mountainbike (these can actually be challenging) or
-                surfing small to medium sized waves - preferably in warm water.
-              </p>
-              <p className='mb-5'>
-                At work I consider myself an extravert person with a keen eye
+                On the job I consider myself an extravert person with a keen eye
                 for user-experience and visual design. I thrive in small teams
                 where communication is open, honest feedback is appreciated and
                 there&apos;s room for humour and legitimate personal interest.
@@ -166,15 +166,14 @@ export default function HomePage() {
               </li>
               <li>Dave van Hoorn</li>
               <li>33 years old</li>
-              <li>Born in the Netherlands</li>
+              <li>Dutch nationality</li>
               <li>Fluent in Dutch, English</li>
-              <li>10+ years experience</li>
+              <li>10+ yrs of web experience</li>
             </ul>
             <ul className='mt-2 mb-6 list-inside list-disc text-sm leading-loose marker:text-gray-300'>
               <li className='mb-2 list-none text-sm font-semibold'>
                 Business details
               </li>
-              <li>Dave van Hoorn</li>
               <li>Meindert Hobbemalaan 8</li>
               <li>3401 NB IJsselstein</li>
               <li>Utrecht, The Netherlands</li>
