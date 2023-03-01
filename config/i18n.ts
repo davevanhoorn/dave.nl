ab5ca963c72c1bf5835d0c7a012f9d9f9be1eca9
Dave(.nl) van Hoorn <yo@dave.nl>
2023-03-01 13:45:15 +0530

    ci: package.json and GitHub actions

--- config/i18n.ts
+++ config/i18n.ts
@@ -5,7 +5,7 @@ export const i18n = {
   locales: ["en", "nl"],
 } as const;
 
-export type Locale = typeof i18n["locales"][number];
+export type Locale = (typeof i18n)["locales"][number];
 
 export type LocaleParams = { locale: Locale };
 
